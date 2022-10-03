990c8f260475c1526e5540dfbff8433fc0431e93
Hoang Hai <haihoang20023@gmail.com>
2022-10-03 16:51:01 +0700

    fix route

--- src/routes/web.route.js
+++ src/routes/web.route.js
@@ -4,10 +4,9 @@ let router = express.Router();
 
 const initWebroute = (app) => {
   router.get("/v1/management", homeController.managementproducts);
+  router.get("/v1/addproduct", homeController.addProduct);
 
-  
   return app.use("/", router);
-
 };
 
 export default initWebroute;
